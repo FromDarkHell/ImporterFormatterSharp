b0f7525973ea74067470c0c7a92f6bf0d436e65e
FromDarkHell <FromDarkHell@DARKHELL>
2019-10-04 19:51:20 -0500

    Add literally all of it

---
+++ ImporterFormatterSharp/FormatImports.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using System.Text.RegularExpressions;
+using Task = System.Threading.Tasks.Task;
+using System.Collections.Generic;
+using EnvDTE;
+using System.IO;
+using EnvDTE80;
+
+namespace ImporterFormatterSharp
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class FormatImports
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("0023e92e-6ce1-4696-93b6-17d2aabb301c");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormatImports"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private FormatImports(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static FormatImports Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in FormatImports's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new FormatImports(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var projects = Projects();
+            foreach (Project p in projects)
+            {
+                foreach (ProjectItem item in p.ProjectItems)
+                {
+                    if (item.ProjectItems.Count > 1)
+                        continue;
+                    string filePath = item.FileNames[0];
+
+                    if (!filePath.EndsWith(".cs")) continue;
+                    if (item.IsOpen) item.Save();
+                    string[] fileLines = File.ReadAllLines(filePath);
+                    List<string> sortedUsings = new List<string>();
+                    foreach (string line in fileLines)
+                        if (Regex.IsMatch(line.ToLower(), "^using [a-z]")) sortedUsings.Add(line);
+                    sortedUsings.Sort((a, b) => a.Length.CompareTo(b.Length));
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        foreach (string import in sortedUsings)
+                            writer.WriteLine(import);
+                        foreach (string line in fileLines)
+                            if (!Regex.IsMatch(line.ToLower(), "^using [a-z]")) writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+
+        public static DTE2 GetActiveIDE()
+        {
+            // Get an instance of currently running Visual Studio IDE.
+            DTE2 dte2 = Package.GetGlobalService(typeof(DTE)) as DTE2;
+            return dte2;
+        }
+        private static IEnumerable<Project> GetSolutionFolderProjects(Project solutionFolder)
+        {
+            List<Project> list = new List<Project>();
+            for (var i = 1; i <= solutionFolder.ProjectItems.Count; i++)
+            {
+                var subProject = solutionFolder.ProjectItems.Item(i).SubProject;
+                if (subProject == null)
+                {
+                    continue;
+                }
+
+                // If this is another solution folder, do a recursive call, otherwise add
+                if (subProject.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    list.AddRange(GetSolutionFolderProjects(subProject));
+                }
+                else
+                {
+                    list.Add(subProject);
+                }
+            }
+            return list;
+        }
+
+        public static IList<Project> Projects()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            Projects projects = GetActiveIDE().Solution.Projects;
+            List<Project> list = new List<Project>();
+            var item = projects.GetEnumerator();
+            while (item.MoveNext())
+            {
+                var project = item.Current as Project;
+                if (project == null)
+                {
+                    continue;
+                }
+
+                if (project.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+                {
+                    list.AddRange(GetSolutionFolderProjects(project));
+                }
+                else
+                {
+                    list.Add(project);
+                }
+            }
+
+            return list;
+        }
+    }
+}
